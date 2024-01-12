94b81a60ad49e863ca44feca6f98ba628e728e7d
ximenagay <xipazgay@gmail.com>
2024-01-12 10:53:52 -0300

    AppTemperatura Verisión 2

--- Temperatura.js
+++ Temperatura.js
@@ -1,7 +1,20 @@
-const temperaturaCelsius = Number(prompt("Ingrese una temperatura en grados Celsius: "))
+function convertirTemperatura(){
 
-let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
-let temperaturaKelvin = temperaturaCelsius + 273.15;
+const temperaturaCelsius = prompt("Ingrese una temperatura en grados Celsius: ")//pedir la temperatura en °C
 
-console.log("temperaturaFahrenheit: " + temperaturaFahrenheit + " F°");
-console.log("temperaturaKelvin: " + temperaturaKelvin + " K");
+if (isNaN(temperaturaCelsius)){//Comprueba si el prompt corresponde a número
+
+    alert("Porfavor, ingresa un valor numérico.");
+    convertirTemperatura();
+    return;
+}
+    //Convertir a fahrenheit y kelvin
+    let temperaturaFahrenheit = (Number(temperaturaCelsius) * 9/5) + 32;
+
+    let temperaturaKelvin = Number(temperaturaCelsius) + 273;
+    
+    console.log(`Temperatura en Fahrenheit: ` + temperaturaFahrenheit + `°F`);
+    console.log(`Temperatura en Kelvin: ` + temperaturaKelvin + `K`);
+
+}
+convertirTemperatura();
